229afa35c71cec36ba4ee6394d34588bf35b20be
zmoog <m.branca@arduino.cc>
2020-02-22 06:56:08 +0100

    Replace setup w/ a new implementation

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
-import setuptools
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages  # noqa: H301
 
-setuptools.setup(setup_requires=['pbr'], pbr=True)
+with open("README.md") as f:
+    long_description = f.read()
+
+NAME = "ifq"
+REQUIRES = ["lxml >= 4.5.0", "requests >= 2.22.0"]
+
+setup(
+    name=NAME,
+    version="0.1.",
+    description="Library to download IFQ issues",
+    author_email="maurizio.branca@gmail.com",
+    url="https://github.com/zmoog/ifq",
+    keywords=[],
+    install_requires=REQUIRES,
+    packages=find_packages(exclude=["test", "tests"]),
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
