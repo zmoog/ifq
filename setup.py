97348dfde7488bad71f665e0bf76fc705c41e7ea
zmoog <m.branca@arduino.cc>
2020-02-22 07:43:30 +0100

    Update setup tools

--- setup.py
+++ setup.py
@@ -9,13 +9,20 @@ REQUIRES = ["lxml >= 4.5.0", "requests >= 2.23.0"]
 
 setup(
     name=NAME,
-    version="0.1.",
-    description="Library to download IFQ issues",
+    version="0.1.0",
+    description="library to download www.ilfattoquotidiano.it issues in PDF",
     author_email="maurizio.branca@gmail.com",
     url="https://github.com/zmoog/ifq",
     keywords=[],
     install_requires=REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
+    classifiers=[  # https://pypi.org/classifiers/
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],    
     include_package_data=True,
     long_description=long_description,
     long_description_content_type="text/markdown",
