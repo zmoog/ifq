b52c839920c348c2e6c60742dec32304b1357cd6
Maurizio Branca <maurizio.branca@elastic.co>
2022-11-08 23:41:00 +0100

    Replace Pipenv with plain pip (#49)
    * Replace Pipenv with plain pip
    
    It now uses setup.py for dependencies only to keep the project DRY.
    
    Relates to: #48
    
    * Add wheel dependency
    
    * Comment pipenv steps
    
    * Upgrade to actions/setup-python@v4
    
    * Add safety for security checking
    
    * Test with plain pytest

--- setup.py
+++ setup.py
@@ -1,29 +1,45 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages  # noqa: H301
+from setuptools import setup
+import os
 
-with open("README.md") as f:
-    long_description = f.read()
+VERSION = "0.3.0"
 
-NAME = "ifq"
-REQUIRES = ["lxml >= 4.5.0", "requests >= 2.23.0"]
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
 
 setup(
-    name=NAME,
-    version="0.2.0",
-    description="library to download www.ilfattoquotidiano.it issues in PDF",
-    author_email="maurizio.branca@gmail.com",
-    url="https://github.com/zmoog/ifq",
-    keywords=[],
-    install_requires=REQUIRES,
-    packages=find_packages(exclude=["test", "tests"]),
-    classifiers=[  # https://pypi.org/classifiers/
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-    ],
-    include_package_data=True,
-    long_description=long_description,
+    name="ifq",
+    description="Library to download www.ilfattoquotidiano.it issues in PDF",
+    long_description=get_long_description(),
     long_description_content_type="text/markdown",
+    author="Maurizio Branca",
+    url="https://github.com/zmoog/ifq",
+    project_urls={
+        "Issues": "https://github.com/zmoog/ifq/issues",
+        "CI": "https://github.com/zmoog/ifq/actions",
+        "Changelog": "https://github.com/zmoog/ifq/releases",
+    },
+    license="Apache License, Version 2.0",
+    version=VERSION,
+    packages=["ifq"],
+    entry_points="""
+        [console_scripts]
+    """,
+    install_requires=[
+        "lxml >= 4.9.1",
+        "requests >= 2.28.1",
+    ],
+    extras_require={
+        "test": [
+            "pytest",
+            "safety",
+            "wheel >= 0.38.3",
+        ],
+    },
+    python_requires=">=3.7",
 )
